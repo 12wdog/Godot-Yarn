ed90876ac49da4d172865d7bdab1c792e7fe05ae
321wCat <wdoghelp@gmail.com>
2026-09-24 20:27:53 -0600

    renamed and reorganized certain files

--- globals/systems/sys_input_handler.gd
+++ globals/systems/sys_input_handler.gd
@@ -7,54 +7,12 @@ var _context_stack: Array[StringName] = []
 var _contexts: Dictionary = {}
 var _global_handlers: Array[GlobalInputHandler] = []
 
-
-func _ready() -> void:
-	process_mode = Node.PROCESS_MODE_ALWAYS
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if _handle_global_input(event):
-		get_viewport().set_input_as_handled()
-		return
-		
-	if _current_context == &"":
-		return
-	
-	var input_context: Node = _contexts.get(_current_context)
-	
-	if input_context == null:
-		return
-		
-	if input_context.handle_input(event):
-		get_viewport().set_input_as_handled()
-
-
-func register_global_handler(handler: GlobalInputHandler) -> void:
-	if handler in _global_handlers:
-		return
-	
-	_global_handlers.append(handler)
-
-
-func unregister_global_handler(handler: Node) -> void:
-	_global_handlers.erase(handler)
-
-
-func _handle_global_input(event: InputEvent) -> bool:
-	for handler : GlobalInputHandler in _global_handlers:
-		if handler.handle_input(event):
-			return true
-		
-	return false
-
-
 func register_context(context_name: StringName, context: Node) -> void:
 	if _contexts.has(context_name):
 		Log.warn("Input context already registered: %s" % context_name)
 		return
 	
 	_contexts[context_name] = context
-
 
 func unregister_context(context_name: StringName) -> void:
 	_contexts.erase(context_name)
@@ -64,30 +22,25 @@ func unregister_context(context_name: StringName) -> void:
 		
 	_context_stack.erase(context_name)
 
+func register_global_handler(handler: GlobalInputHandler) -> void:
+	if handler in _global_handlers:
+		return
+	
+	_global_handlers.append(handler)
+
+func unregister_global_handler(handler: GlobalInputHandler) -> void:
+	_global_handlers.erase(handler)
 
 func set_context(context_name: StringName) -> void:
 	_context_stack.clear()
 	_set_context(context_name)
-
-func _set_context(context_name: StringName) -> void:
-	if context_name == &"":
-		_current_context = &""
-		return
 	
-	if not _contexts.has(context_name):
-		Log.warn("Cannot activate unregistered input context: %s" % context_name)
-		return
-	
-	_current_context = context_name
-
 func get_context() -> StringName:
 	return _current_context
 
-
 func is_context(context_name: StringName) -> bool:
 	return _current_context == context_name
-
-
+	
 func push_context(context_name: StringName) -> void:
 	if not _contexts.has(context_name):
 		Log.warn("Cannot push unregistered input context: %s" % context_name)
@@ -112,3 +65,43 @@ func clear_context_stack() -> void:
 
 func get_context_stack() -> Array[StringName]:
 	return _context_stack.duplicate()
+
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if _handle_global_input(event):
+		get_viewport().set_input_as_handled()
+		return
+		
+	if _current_context == &"":
+		return
+	
+	var input_context: Node = _contexts.get(_current_context)
+	
+	if input_context == null:
+		return
+		
+	if input_context.handle_input(event):
+		get_viewport().set_input_as_handled()
+
+
+func _handle_global_input(event: InputEvent) -> bool:
+	for handler : GlobalInputHandler in _global_handlers:
+		if handler.handle_input(event):
+			return true
+		
+	return false
+
+func _set_context(context_name: StringName) -> void:
+	if context_name == &"":
+		_current_context = &""
+		return
+	
+	if not _contexts.has(context_name):
+		Log.warn("Cannot activate unregistered input context: %s" % context_name)
+		return
+	
+	_current_context = context_name
